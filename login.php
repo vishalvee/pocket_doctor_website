51af600a053e0e31e5dd583eb96b658333dac791
Vishal <124968089+vishalvee@users.noreply.github.com>
2024-02-20 20:23:50 +0530

    A_Login_Page_of_PocketDoctor
    An Admin Login Form where Admin can login there Account to Access the Admin_Page.

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css?=v4">
+    <title>Admin_Login_Page</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="myform">
+        <form>
+            <h2>Log In</h2>
+            <div class="userlogo">
+             <img src="user.png" alt="user" width="20px">
+            </div>
+            <input type="text" placeholder="Admin Name">
+            <div class="passwordlogo">
+             <img src="password.png" alt="user" width="20px">
+            </div>
+            <input type="password" placeholder="Password">
+            <button type="submit"><a href="http://localhost/adminpanelvee/admin_page.php"> LogIn</a></button>
+        </form>
+    </div>
+    <div class="image">
+        <img src="blackdp2.png" alt="lff" width="300px">
+    </div>
+</div>
+
+    
+</body>
+</html>
